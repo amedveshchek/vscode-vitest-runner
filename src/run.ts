9f30fbefb31e1b9b8ef66d1f53332297242c3653
Alex Medveshchek <alex.medveshchek@gmail.com>
2023-08-12 11:26:19 +0200

    Heavily refactored the src/run.ts to make it work and stable

--- src/run.ts
+++ src/run.ts
@@ -1,49 +1,83 @@
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-function buildVitestArgs(text: string) {
-    return ['vitest', 'run', '-t', text];
+function getNodeProjectRootPath(filePath: string): string {
+    let fileDir = path.dirname(filePath);
+    let rootPath = fileDir;
+    let backHops = [];
+    const packageJsonFile = () => path.join(rootPath, 'package.json')
+
+    let i = 0;
+    const maxBackHops = 20;
+    while (i < maxBackHops && !fs.existsSync(packageJsonFile())) {
+        backHops.push('..');
+        rootPath = path.join(fileDir, ...backHops);
+        i++;
+    }
+    if (i >= maxBackHops) {
+        throw Error(`can't find the root of the project for filePath=${filePath}, last tried rootPath=${rootPath}`);
+    }
+
+    return path.resolve(rootPath);
 }
 
-function buildCdArgs(path: string) {
-    return ['cd', path];
+function getTestCasePatternRegexp(testCaseName: string): string {
+    return ` ${testCaseName}($| )`; // TODO: make it better, determine what is the real test case name in vitest.mjs
 }
 
-export function runInTerminal(text: string, filename: string) {
-    const casePath = path.dirname(filename);
-    const terminal = vscode.window.createTerminal(`vitest - ${text}`);
+function buildDebugConfig(props: {
+    testCaseName: string,
+    filename: string,
+    noDebug?: boolean,          // https://microsoft.github.io/debug-adapter-protocol//specification.html
+    notJustMyCode?: boolean,    // https://code.visualstudio.com/docs/python/debugging#_justmycode
+    preLaunchTask?: string,     // https://code.visualstudio.com/docs/editor/debugging#_launchjson-attributes
+}): vscode.DebugConfiguration {
+    const cwd = getNodeProjectRootPath(props.filename);
 
-    const casePathStr = JSON.stringify(casePath);
-    const caseNameStr = JSON.stringify(text);
+    const justMyCode = !(props.notJustMyCode ?? false);
 
-    const cdArgs = buildCdArgs(casePathStr);
-    terminal.sendText(cdArgs.join(' '), true);
-
-    const vitestArgs = buildVitestArgs(caseNameStr);
-    const npxArgs = ['npx', ...vitestArgs];
-    terminal.sendText(npxArgs.join(' '), true);
-    terminal.show();
-}
-
-function buildDebugConfig(
-    cwd: string,
-    text: string
-): vscode.DebugConfiguration {
+    // https://vitest.dev/guide/debugging.html
     return {
-        name: 'Debug vitest case',
-        request: 'launch',
-        runtimeArgs: buildVitestArgs(text),
-        cwd,
-        runtimeExecutable: 'npx',
-        skipFiles: ['<node_internals>/**'],
-        type: 'pwa-node',
-        console: 'integratedTerminal',
-        internalConsoleOptions: 'neverOpen'
-    };
+        "type": "node",
+        "request": "launch",
+        "name": "Vitest",
+        "autoAttachChildProcesses": true,
+        "skipFiles": [
+            "<node_internals>/**",
+            "**/node_modules/**"
+        ],
+        "program": `${cwd}/node_modules/vitest/vitest.mjs`,
+        "args": [
+            "run",
+            "${relativeFile}",
+            "--testNamePattern",
+            getTestCasePatternRegexp(props.testCaseName),
+        ],
+        "smartStep": true,
+        "preLaunchTask": props.preLaunchTask, // "${defaultBuildTask}",
+        "console": "integratedTerminal",
+        "outFiles": [
+            "${workspaceFolder}/out/**/*.js"
+        ],
+        "noDebug": props.noDebug ?? false,
+        "justMyCode": justMyCode,
+    }
 }
 
-export function debugInTermial(text: string, filename: string) {
-    const casePath = path.dirname(filename);
-    const config = buildDebugConfig(casePath, text);
+export function runInTerminal(testCaseName: string, filename: string) {
+    const config = buildDebugConfig({
+        testCaseName: testCaseName,
+        filename: filename,
+        noDebug: true,
+    });
+    vscode.debug.startDebugging(undefined, config);
+}
+
+export function debugInTermial(testCaseName: string, filename: string) {
+    const config = buildDebugConfig({
+        testCaseName: testCaseName,
+        filename: filename,
+    });
     vscode.debug.startDebugging(undefined, config);
 }
